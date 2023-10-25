ed59ed65e1a0a92f89bd6a08f381e216cb04916b
gtobias22 <gabriel_tobias@outlook.com>
2023-10-25 13:19:47 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,15 +1,3 @@
-
-''''Código desenvolvido no intensivão de Python, organizado pela equipe da Hashtag Programação.
-
-O código tem o objetivo de automatizar tarefas repetitivas e nesse caso estamos simulado o cadastro de produtos,
-dentro de um site, utilizando uma base de dados de produtos.
-
-Utilizamos a biblioteca pyautogui para controlarmos o mouse, teclado e tela, a biblioteca time para controlarmos,
-o tempo entre nossas ações e a biblioteca pandas, para integramos com a nossa base de dados de produtos, 
-para serem cadastrada no sistema.
- '''
-
-
 #Importação das bibliotecas
 import pyautogui
 import time
@@ -62,7 +50,7 @@ for linha in tabela.index:
 
     #Clicando no primeiro campo - código
     pyautogui.click(x=1756, y=264)
-    
+
 
     # Cadastrando produtos
 
